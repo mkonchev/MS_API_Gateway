3cbfec6a90c4a32a9beb64cf64617d8cdc81b40a
mkonchev <maksim1901m@gmail.com>
2025-07-04 12:44:30 +0300

    add /users/register

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ app = FastAPI()
 
 ORDER_SERVICE = "http://order"
 CATALOG_SERVICE = "http://catalog"
+USER_SERVICE = "http://users"
 
 
 @app.get("/")
@@ -21,9 +22,26 @@ async def get_products():
 
 @app.post("/order/create")
 async def craete_order(product_id: int, quantity: int):
-    async with httpx.AsyncClient as client:
+    async with httpx.AsyncClient() as client:
         responce = await client.post(
             f"{ORDER_SERVICE}/orders/",
             json={"product_id": product_id, "quantity": quantity}
+        )
+    return responce.json()
+
+
+@app.post("/users/register")
+async def register(
+                email: str,
+                username: str,
+                password: str,
+):
+    async with httpx.AsyncClient(follow_redirects=True) as client:
+        responce = await client.post(
+            f"{USER_SERVICE}/users/register/",
+            json={"email": email,
+                  "username": username,
+                  "password": password,
+                  "active": True}
         )
     return responce.json()
