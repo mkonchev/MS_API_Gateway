e649ed3a0d47857a8ffd110806052eef5abf2655
mkonchev <maksim1901m@gmail.com>
2025-07-10 11:56:05 +0300

    update order create (autofield 'customer')

--- app/order_service/order.py
+++ app/order_service/order.py
@@ -9,13 +9,26 @@ router = APIRouter(prefix="/order", tags=["Order"])
 
 load_dotenv()
 ORDER_SERVICE = os.getenv("ORDER_SERVICE")
+USER_SERVICE = os.getenv("USER_SERVICE")
 
 
 @router.post("/create")
-async def create_order(order: Order):
+async def create_order(token: str, order: Order):
+    temp_order = order.model_dump(exclude="customer")
+
+    async def get_email(token: str):
+        async with httpx.AsyncClient() as client:
+            response = await client.get(
+                f"{USER_SERVICE}/users/me?token={token}"
+            )
+            user_data = response.json()
+        return user_data["email"]
+
+    temp_order["customer"] = await get_email(token)
+
     async with httpx.AsyncClient() as client:
         response = await client.post(
             f"{ORDER_SERVICE}/orders/",
-            json=order.model_dump()
+            json=temp_order
         )
     return response.json()
--- app/order_service/schemas/order.py
+++ app/order_service/schemas/order.py
@@ -4,6 +4,6 @@ from app.order_service.schemas.orderitem import OrderItem
 
 
 class Order(BaseModel):
-    customer: str = Field(default=..., description="Покупатель")
+    customer: str = Field(default=None, description="Покупатель")
     goods: List[OrderItem] = Field(default=..., description="Товары")
-    status: str = Field(default=..., description="Статус заказа")
+    status: str = Field(default="ok", description="Статус заказа")
